dac51ea0d59e9d1a0ab9a255113ee03e6bb789b7
DavidGP201770 <davicho201170@gmail.com>
2018-02-13 21:20:25 -0600

    Primera actualizacion :3

---
+++ src/main/java/org/unitec/finalheorku/FinalHeorkuApplication.java
@@ -0,0 +1,12 @@
+package org.unitec.finalheorku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalHeorkuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalHeorkuApplication.class, args);
+	}
+}
